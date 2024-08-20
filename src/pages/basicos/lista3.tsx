5e3be9e39936da3ea914e3be721ac30b0e2e1e60
felipedestroo <fdestrocontact@gmail.com>
2024-08-19 21:23:03 -0300

    feat: for resolution

---
+++ src/pages/basicos/lista3.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-key */
+function gerarLista(final = 10) {
+    const lista = []
+    for (let i = 1; i <= final; i++) {
+        lista.push(<span>{i},</span>)
+    }
+    return lista
+}
+
+export default function lista() {
+
+    return(
+        <div>
+            {gerarLista(10)}
+        </div>
+    )
+    
+    
+
+}
